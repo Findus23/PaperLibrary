167b8b4b2c158737cbe857bd1727d5032b90aa16
Lukas Winkler <git@lw1.at>
2025-02-06 22:04:50 +0100

    force unix line ending in bibtex

--- library/utils/bibtex.py
+++ library/utils/bibtex.py
@@ -3,10 +3,10 @@ from django.db.models import QuerySet
 from library.models import Paper
 
 
-def papers_to_bibtex_file(query: QuerySet[Paper])->str:
+def papers_to_bibtex_file(query: QuerySet[Paper]) -> str:
     code = "% Encoding: UTF-8\n\n"
     for paper in query.order_by("citation_key"):
-        code += paper.bibtex + "\n\n"
+        code += paper.bibtex.replace("\r\n", "\n") + "\n\n"
 
     code += "@Comment{jabref-meta: databaseType:biblatex;}\n"
     return code
